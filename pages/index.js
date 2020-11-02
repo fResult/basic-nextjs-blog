c73fa3d82999f40096acc62b2e12e3d149beb3c8
Sila Setthakan-anan <sila@MacBook-Pro-khxng-Newman.local>
2020-11-02 15:05:16 +0700

    1st commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
@@ -68,7 +68,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         main {
           padding: 5rem 0;
           flex: 1;
@@ -77,7 +76,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         footer {
           width: 100%;
           height: 100px;
@@ -86,49 +84,40 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         footer img {
           margin-left: 0.5rem;
         }
-
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
-
         a {
           color: inherit;
           text-decoration: none;
         }
-
         .title a {
           color: #0070f3;
           text-decoration: none;
         }
-
         .title a:hover,
         .title a:focus,
         .title a:active {
           text-decoration: underline;
         }
-
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
-
         .title,
         .description {
           text-align: center;
         }
-
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
-
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -137,17 +126,14 @@ export default function Home() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
-
         .grid {
           display: flex;
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-
           max-width: 800px;
           margin-top: 3rem;
         }
-
         .card {
           margin: 1rem;
           flex-basis: 45%;
@@ -159,29 +145,24 @@ export default function Home() {
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
-
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
-
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
-
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
-
         .logo {
           height: 1em;
         }
-
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -199,7 +180,6 @@ export default function Home() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
         }
