49e93068927f2ccedb2c0a162b8f86f2986e6133
fResult <styxmaz@gmail.com>
2020-11-03 16:08:30 +0700

    Displayed Post data in Post component (`pages/posts/[id].js` file)

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,18 +2,25 @@ import React from 'react'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostDataById } from '../../lib/posts'
 
-const Post = () => {
-  return <Layout></Layout>
+const Post = ({ post }) => {
+  const { id, title, date } = post
+  return (
+    <Layout>
+      {title} <br/>
+      {id} <br/>
+      {date}
+    </Layout>
+  )
 }
 
 export default Post
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
-  return { paths, fallback: false }
+  return { paths, fallback: true }
 }
 
 export async function getStaticProps({ params }) {
   const postData = getPostDataById(params.id)
-  return { props: { postData } }
+  return { props: { post: postData } }
 }
