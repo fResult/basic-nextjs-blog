bf641610e15dd15db303034278379bc3fb51849f
fResult <styxmaz@gmail.com>
2020-11-03 15:25:12 +0700

    Added getPostDataById() into `lib/posts.js` file and implements getStaticProps before pass props into `pages/posts/[id].js` (Post) component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostDataById } from '../../lib/posts'
 
 const Post = () => {
   return <Layout></Layout>
@@ -10,11 +10,10 @@ export default Post
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
+  return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
+  const postData = getPostDataById(params.id)
+  return { props: { postData } }
 }
