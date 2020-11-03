2d13c4a9516d535a50726bd486a5d2a31d2342cc
fResult <styxmaz@gmail.com>
2020-11-03 11:54:24 +0700

    Added `lib/posts.js` file to read data in Markdown file (use `gray-matter` lib to help) and Added config webpack in `next.config.js` file to solve problem `Cannot find 'fs' module`

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ const path = require('path')
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
   }
 }
