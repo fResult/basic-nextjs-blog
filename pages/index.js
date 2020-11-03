65c3c6d6c67c0b5bf9c7c392b9dc136affcb173c
fResult <styxmaz@gmail.com>
2020-11-03 12:29:23 +0700

    Reformat code

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,36 @@
 import Head from 'next/head'
 import React from 'react'
+
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p className="bg-blue-100">
-          Hello I'm Korn. <br />
-          You can also tell me short, that's `Sila`. <br />
-          My site is called&nbsp;
-          <a href="https://fResult.dev" target="_blank">
-            fResult website.
-          </a>{' '}
-          <br />
-          Thanks.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>. )
-        </p>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title} <br/>
+              {id} <br/>
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
